cca924e4471cb9b5d924df48ba6d285985106f33
Francesca Ye <francescaye20@gmail.com>
2024-03-28 10:27:14 -0400

    data cleaning preamble updated

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleaning Marriage Ceremony Data
+# Author: Francesca Ye
+# Date: 28 March 2024
+# Contact: francesca.ye@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Access to the raw data file `marriagesworkbook2020.xlsx`
 
 #### Workspace setup ####
 library(tidyverse)
