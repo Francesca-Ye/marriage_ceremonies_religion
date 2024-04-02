a847fa8fa4e29f96efd3e4de9c04fd59c98236b1
Francesca Ye <francescaye20@gmail.com>
2024-04-02 19:54:46 -0400

    download data script and files added

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,32 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(readxl)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Get the Marriages in England and Wales Dataset (2020 edition)
+# Code referenced from: https://piazza.com/class/lrja9wmteoj28u/post/10
+download.file(url = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/marriagecohabitationandcivilpartnerships/datasets/marriagesinenglandandwales2013/2020/marriagesworkbook2020.xlsx", destfile = "~/marriage_ceremonies_religion/data/raw_data/00-marriagesworkbook2020.xlsx")
 
+# Download ceremony statistics on opposite-sex marriages
+opposite_sex_data <- read_excel("~/marriage_ceremonies_religion/data/raw_data/00-marriagesworkbook2020.xlsx", sheet = "1b")
 
+# Download ceremony statistics on same-sex marriages
+same_sex_data <- read_excel("~/marriage_ceremonies_religion/data/raw_data/00-marriagesworkbook2020.xlsx", sheet = "1c")
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Save ceremony statistics on opposite-sex marriages as a CSV
+write_csv (
+  x = opposite_sex_data,
+  file = "~/marriage_ceremonies_religion/data/raw_data/01-raw_opposite_sex_data.csv"
+)
+
+# Sane ceremony statistics on same-sex marriages as a CSV
+write_csv (
+  x = same_sex_data,
+  file = "~/marriage_ceremonies_religion/data/raw_data/02-raw_same_sex_data.csv"
+)
 
          
